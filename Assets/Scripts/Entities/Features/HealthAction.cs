aaff48171c0144b1df83e3cc1dc4a6b7a0a3907a
Alejandro Medina <almpratto@gmail.com>
2025-03-11 20:49:11 -0500

    Life & HealthActions

---
+++ Assets/Scripts/Entities/Features/HealthAction.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthAction : MonoBehaviour
+{
+    public Life life;
+
+    public HealthEvent[] AttachablkeEvents;
+
+    private void Awake()
+    {
+        if (life == null && gameObject.GetComponent<Life>()) life = gameObject.GetComponent<Life>();
+    }
+
+    public void CheckActions()
+    {
+        if (life == null) return;
+        if(AttachablkeEvents.Length>0)
+        {
+            for(int i = 0; i < AttachablkeEvents.Length; i++)
+            {
+                if (AttachablkeEvents[i].invoked==false && life.GetHealthPercentage() <= AttachablkeEvents[i].healthPercentage)
+                {
+                    AttachablkeEvents[i].EvokeAction.Invoke();
+                    AttachablkeEvents[i].invoked = true;
+                }
+            }
+        }
+    }
+
+    public void RebuildActions()
+    {
+        for (int i = 0; i < AttachablkeEvents.Length; i++)
+        {
+            AttachablkeEvents[i].invoked = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (life != null) life.HealthUpdate += (int value) => CheckActions();
+    }
+
+    private void OnDisable()
+    {
+        if (life != null) life.HealthUpdate -= (int value) => CheckActions();
+    }
+}
+[System.Serializable]
+public class HealthEvent
+{
+    [Range(0f, 100f)]
+    public float healthPercentage;
+    public UnityEvent EvokeAction;
+    public bool invoked;
+}
---
+++ Assets/Scripts/Entities/Features/Life.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Life : MonoBehaviour
+{
+    public event Action<int> HealthUpdate = delegate {};
+    public event Action Dead = delegate {};
+
+    public int maxHealth = 100;
+
+    public int currentHealth;
+
+    public void GainHealth(int amount)
+    {
+        amount = (int)MathF.Abs(amount);
+        if (currentHealth + amount >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += amount;
+        }
+        HealthUpdate.Invoke(amount);
+    }
+
+    public void LooseHealth(int amount)
+    {
+        amount = (int)MathF.Abs(amount);
+        if (currentHealth - amount <= 0)
+        {
+            currentHealth = 0;
+        }
+        else
+        {
+            currentHealth -= amount;
+        }
+        HealthUpdate.Invoke(-amount);
+    }
+
+    public float GetHealthPercentage()
+    {
+        return (100 * currentHealth) / maxHealth;
+    }
+
+    public void SetHealthPercentage(float percentage)
+    {
+        
+        int newHealth = (int)(maxHealth * percentage) / 100;
+
+        int changeAmout = newHealth - currentHealth;
+        HealthUpdate.Invoke(changeAmout);
+    }
+
+
+
+    //DeadSubscription
+    public void OnEnable()
+    {
+        currentHealth = maxHealth;
+
+        HealthUpdate += (int amount) =>
+        { 
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                Dead.Invoke();
+            }      
+        };
+    }
+
+    public void OnDisable()
+    {
+        HealthUpdate -= (int amount) =>
+        {
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                Dead.Invoke();
+            }
+        };
+    }
+}
