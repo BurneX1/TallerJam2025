db8ed1e1a2c0e1b6d49b7dcc816bd9d0dd478e08
Alejandro Medina <almpratto@gmail.com>
2025-03-16 18:23:27 -0500

    LigthEnemies

---
+++ Assets/Scripts/Entities/Features/ColorVisualizer.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.Rendering.Universal;
+
+public class ColorVisualizer : MonoBehaviour
+{
+
+    public IncomeRuneChecker checker;
+
+    public LigthColorGroup[] runeColors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GetComponent<IncomeRuneChecker>()) checker = GetComponent<IncomeRuneChecker>();
+        //SetColors();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        if (checker) checker.OnRuneDamage += (RuneType type, int dmg) => { RevokeColors(type); };
+        SetColors();
+    }
+
+    private void OnDisable()
+    {
+        if (checker) checker.OnRuneDamage -= (RuneType type, int dmg) => { RevokeColors(type); };
+    }
+    public void SetColors()
+    {
+        ClearRunes();
+
+        if (checker)
+        {
+            int i = 0;
+
+            foreach(RuneData data in checker.vulnerableRunes)
+            {
+                runeColors[i].rune = data;
+            }
+        }
+
+        RefreshColors();
+    }
+
+    public void RevokeColors(RuneType type)
+    {
+        foreach (LigthColorGroup ligths in runeColors)
+        {
+            if (ligths.rune.RuneType == type)
+            {
+                ligths.rune = null;
+                RefreshColors();
+                return;
+            }
+        }
+
+    }
+
+    public void RefreshColors()
+    {
+        foreach(LigthColorGroup ligths in runeColors)
+        {
+            if(ligths.rune == null)
+            {
+                for(int i = 0; i < ligths.lights.Length; i++)
+                {
+                    ligths.lights[i].color= new Vector4(0,0,0,0);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < ligths.lights.Length; i++)
+                {
+                    ligths.lights[i].color = ligths.rune.RuneColor;
+
+                }
+            }
+        }
+
+    }
+
+    public void ClearRunes()
+    {
+        foreach (LigthColorGroup ligths in runeColors)
+        {
+            ligths.rune = null;
+        }
+    }
+}
+[System.Serializable]
+public class LigthColorGroup
+{
+    public RuneData rune;
+    public Light2D[] lights;
+}
